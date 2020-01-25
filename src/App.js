79f605ee8b2006e1a791abfa54bd3fbe9f0418ba
Michal Púchy <puchy.michal@gmail.com>
2020-01-25 15:09:10 +0100

    Fetch movies data, bootstrap, carousel, responsive

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Movies from './components/Movies'
+import Alert from 'react-bootstrap/Alert';
+import { useWindowSize } from './utils/events_helper'
+
+import './App.css'
 
 function App() {
+  const [ movies, setMovies ] = useState([])
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios
+          .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_BROWSER_API_KEY}`)
+          setMovies(response.data.results)
+          //.get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.REACT_APP_MOVIE_BROWSER_API_KEY}`)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchMovies()
+  }, [])
+  
+  const [width, height] = useWindowSize()
+
+  const statisticStyle = {
+    padding: 0,
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>Window size: {width} x {height}</span>
+      <main>
+        <Alert key='primary' variant='danger'>
+          This is a alert—check it out!
+        </Alert>
+        <h3>Popular Movies</h3>
+        <div style={statisticStyle}>
+          <Movies movies={movies} width={width} handler={() => onclick(alert('bubu'))}/>
+        </div>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Movies.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import FigureImage from 'react-bootstrap/FigureImage'
+import Carousel from 'react-bootstrap/Carousel'
+import _ from 'lodash'
+
+const Movies = ({ movies, width, handler }) => {
+  const image = { width: 171, height: 180 }
+  const chunksAmount = Math.floor(width / image.width) 
+  const imageBaseUrl = 'http://image.tmdb.org/t/p/w342/'
+  const moviesChunks = _.chunk(movies, chunksAmount)
+
+  return (
+    <Carousel>
+      { moviesChunks.map((chunk, i) => {
+        return (
+          <Carousel.Item key={`carousel_${i}`}>
+            { chunk.map((film, j) => {
+              return (
+                <FigureImage key={`image_${j}`}
+                  width={image.width}
+                  height={image.height}
+                  alt={`${image.width}x${image.height}`}
+                  src={`${imageBaseUrl}${film.poster_path}`}
+                />
+              )
+            })}
+          </Carousel.Item>
+        )
+      })}
+    </Carousel>
+  )
+}
+
+export default Movies
