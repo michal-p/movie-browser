a364f3b4f1bf55eb3b56291bbb2f28601b910e1b
Michal Púchy <puchy.michal@gmail.com>
2020-05-25 17:29:53 +0200

    FE node_modules with express and gitignore changes

--- be-app/index.js
+++ be-app/index.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+let notes = [
+  ...
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
